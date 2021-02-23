f580bc955b404f29b30df6ebb46feaf86f1cf357
Erik Huang <erikhuangdrums@gmail.com>
2021-02-23 09:45:47 -0800

    commit tree cc122ad16b35824cbadd5623e04c640ac77c57f5

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Add your code here
+const submitData = (name, email) => {
+    const formData = {
+        name: name,
+        email: email
+    }
+    return fetch('http://localhost:3000/users', {
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        "body": JSON.stringify(formData)
+    })
+    .then(resp => resp.json())
+    .then(json => document.body.innerHTML = json.id)
+    .catch(err => document.body.innerHTML = err.message)
+}
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// };
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+// fetch("http://localhost:3000/dogs", configObj)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
